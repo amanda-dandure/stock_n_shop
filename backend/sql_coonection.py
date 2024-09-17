0f4c91e5fa5de257b17d85e4ec43cee8c7e87cfa
Amandah <122104257+amanda-dandure@users.noreply.github.com>
2024-09-17 17:22:14 +0200

    add function to establish MySQL connection

--- backend/sql_coonection.py
+++ backend/sql_coonection.py
@@ -7,7 +7,7 @@ def get_sql_connection():
   print("Opening the MySQL connection")
   global __cnx
 
-  if __cnx is None: #if the __cnx variable is currently None, indicating that a connection has not been established yet
+  if __cnx is None: #if the __cnx variable is currently none, indicating that a connection has not been established yet
     __cnx = mysql.connector.connect(user='root', password='AAmm!!99', database='ss')
 
   return __cnx #returns the __cnx variable, which now contains the established database connection object
